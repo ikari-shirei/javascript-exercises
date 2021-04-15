811677fd9f1a318df2b10fbfd5d57b65051c2796
ikari-shirei <algrsyu@protonmail.com>
2021-04-16 01:11:38 +0300

    Completed.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,49 @@
-function add () {
-	
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract () {
-	
+function subtract(num1, num2) {
+  if (num1 >= num2) {
+    return num1 - num2;
+  } else {
+    return num2 - num1;
+  }
 }
 
-function sum () {
-	
+function sum(num) {
+  return num.reduce((x, y) => {
+    return x + y;
+  }, 0);
 }
 
-function multiply () {
-	
+function multiply(num) {
+  return num.reduce((x, y) => {
+    return x * y;
+  });
 }
 
-function power() {
-	
+function power(num1, num2) {
+  return num1 ** num2;
 }
 
-function factorial() {
-	
+function factorial(num) {
+  if (num === 0 || num === 1) return 1;
+  let arr = [];
+  for (let i = num; i > 0; i--) {
+    arr.push(i);
+  }
+  return arr.reduce((x, y) => {
+    return x * y;
+  });
 }
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
+
+console.log(factorial(10));
